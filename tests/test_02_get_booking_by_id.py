a531197440b0ca8d730f946c0b2fc93c49c93120
Pappu Sagar <automationspecialist99@gmail.com>
2026-02-04 09:11:18 +0530

    Adding Negative Test case for fetching booking with Non existing Id

--- tests/test_02_get_booking_by_id.py
+++ tests/test_02_get_booking_by_id.py
@@ -7,3 +7,8 @@ def test_get_booking_id(api_client,config):
     responses = api_client.get(config["booking"] + "/" + str(config["retrieve_booking_id"]))
     assert_status_code(responses, config["success_status_code"])
     get_logger().info("Get booking details successfully")
+
+def test_get_booking_with_non_existing_id(api_client,config):
+    responses = api_client.get(config["booking"] + "/" + str(config["retrieve_booking_non_existing_id"]))
+    assert_status_code(responses, config["error_status_code"])
+    get_logger().info("Unable to get the booking due to Non existing ID")
